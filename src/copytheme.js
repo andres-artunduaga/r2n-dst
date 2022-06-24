b57c1f450ffc8fefea139214c51354e4ca827343
Andres Artunduaga <andres.artunduaga@outlook.com>
2022-06-23 22:35:43 -0500

    chore: update docs and add namespace parameter

--- src/copytheme.js
+++ src/copytheme.js
@@ -7,12 +7,13 @@ const findup = require('findup-sync');
 
 /**
  * Creates the $schema path from where the theme file is created
+ * @param {string} destination relative path to destination folder
  * @returns string
  */
-function getSchemaPath() {
+function getSchemaPath(destination) {
     const NODE_MODULES_DIR_PATH = findup('node_modules', { cwd: path.resolve('./') });
     const R2N_DST_SCHEMA_PATH = '@r2n/dst/schema/dst.schema.json';
-    return `${NODE_MODULES_DIR_PATH}/${R2N_DST_SCHEMA_PATH}`;
+    return path.relative(destination, `${NODE_MODULES_DIR_PATH}/${R2N_DST_SCHEMA_PATH}`);
 }
 
 /**
@@ -28,10 +29,20 @@ function getSchemaFileObj(themeName = 'default') {
     return themeFile;
 }
 
+/**
+ * Return the theme file name based on the theme name
+ * @param {*} themeName theme name
+ * @returns string
+ */
 function getThemeFileName(themeName = 'default') {
     return `${themeName}.dst.json`;
 }
 
+/**
+ * This returns the full path where the dst file will live
+ * @param {*} destination relative folder path where the dst file will live
+ * @returns string
+ */
 function getDestinationPath(destination = './') {
     return path.resolve(destination);
 }
@@ -39,17 +50,19 @@ function getDestinationPath(destination = './') {
 function main() {
     const args = minimist(process.argv);
 
-    const { theme, dest } = args;
+    const { theme, dest, ns } = args;
 
     const DESTINATION = dest ?? './';
     const THEME_NAME = theme ?? 'default';
+    const NAMESPACE = ns ?? 'r2n';
     const DESTINATION_DIR_PATH = getDestinationPath(DESTINATION);
     const DESTIONATION_FILE_PATH = `${DESTINATION_DIR_PATH}/${getThemeFileName(theme)}`;
 
     let themeFile = getSchemaFileObj(THEME_NAME);
 
     // Replace $schema using the relative path to the schema
-    themeFile['$schema'] = path.relative(DESTINATION, getSchemaPath());
+    themeFile['$schema'] = getSchemaPath(DESTINATION);
+    themeFile['metadata']['namespace'] = NAMESPACE;
 
     // Create destination directory if it does not exists
     if (!fs.existsSync(DESTINATION_DIR_PATH)) {
