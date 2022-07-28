0e9d149b0b208dcf22ff111b84693d48a815da85
Andres Artunduaga <andres.artunduaga@outlook.com>
2022-07-27 20:01:52 -0500

    chore: add usage examples

--- src/scripts/help.ts
+++ src/scripts/help.ts
@@ -11,22 +11,27 @@ function getHelpItems(): HelpItem[] {
         {
             options: '--init',
             description: 'Generate empty DST file unless used with --theme',
+            usage: "r2ndst --init"
         },
         {
             options: '--destination',
             description: 'Place where the files are gonna be created',
+            usage: "r2ndst --init --destination ./awesome-dst-folder"
         },
         {
             options: '--schema',
             description: 'Creates a copy of the schema that can be modified',
+            usage: "r2ndst --schema"
         },
         {
             options: '--help',
             description: 'Display this information',
+            usage: "r2ndst --help"
         },
         {
             options: '--theme',
             description: 'Add themes',
+            usage: "r2ndst --theme default"
         }
     ];
 }
